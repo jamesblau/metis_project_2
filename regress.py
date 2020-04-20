f1e3fe8ef732ba01e1207d2aecda0920c36704d6
James Blau <jamesblau@gmail.com>
2020-04-20 13:24:29 -0400

    Clean up and comment regress.py.

--- regress.py
+++ regress.py
@@ -3,13 +3,15 @@ import pickle
 import numpy as np
 import pandas as pd
 import seaborn as sns
-from sklearn.model_selection import train_test_split, KFold
-from sklearn.linear_model import LinearRegression, Ridge
-from sklearn.preprocessing import StandardScaler, PolynomialFeatures
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LassoCV, lars_path
+from sklearn.metrics import r2_score
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', 100)
 
+# Grab our fights
 with open("pickles/merged.pickle", 'rb') as to_read:
     merged = pickle.load(to_read)
 
@@ -18,6 +20,19 @@ merged['male'] = pd.get_dummies(merged['male'], drop_first=True)
 # Let's not bet on tomato cans
 merged = merged[merged['fight_count_sum'] < 100]
 
+###########################################
+## First Plot: Distribution of Fight Counts
+###########################################
+fight_counts_agg = df\
+    .groupby(['f1'])\
+    .agg({
+        'fight_count': 'first',
+    })
+fight_counts_agg.hist()
+
+# Our dataset clearly skews towards fighters with very few fights
+
+# Let's make it more balanced by fight counts, and gender too, why not
 men = merged[merged['male'] == 1]
 women = merged[merged['male'] == 0]
 
@@ -47,26 +62,9 @@ balanced_women = pd.concat([df for quantile, df in women_by_quantile.items()])
 
 balanced = pd.concat([balanced_men, balanced_women])
 
-#####################
-
-sns.set()
-
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression, Lasso, LassoCV, Ridge, RidgeCV
-from sklearn.metrics import r2_score
-
-cols1 = ['fight_count', 'height', 'class', 'male', 'fight_count_2', 'time']
-cols2 = ['height_2', 'fight_count_dif', 'fight_count_sum', 'height_dif', 'height_sum', 'time']
-cols3 = ['win_rate_dif', 'win_rate_sum', 'loss_rate_dif', 'loss_rate_sum', 'fastest_win_dif', 'time']
-cols4 = ['fastest_win_sum', 'fastest_loss_dif', 'fastest_loss_sum', 'avg_win_time_dif', 'time']
-cols5 = ['avg_win_time_sum', 'avg_loss_time_dif', 'avg_loss_time_sum', 'fights_quantile', 'time']
-sumcols = [col for col in balanced.columns if 'sum' in col]
-difcols = [col for col in balanced.columns if 'dif' in col]
-
-X = balanced.drop(['time'], 1)
-Xsum = balanced.drop(difcols + ['time'], 1)
-Xdif = balanced.drop(sumcols + ['time'], 1)
-y = balanced['time']
+###########################################
+## EDA (a little of it)
+###########################################
 
 # There were too many features at this point to easily look at the pairplot
 # (Or even to quickly generate one)
@@ -80,114 +78,79 @@ cols = ['height_2', 'fight_count_dif', 'fight_count_sum', 'height_dif', 'height_
 # cols = ['avg_win_time_sum', 'avg_loss_time_dif', 'avg_loss_time_sum', 'fights_quantile', 'time']
 sns.pairplot(balanced[cols])
 
-#####################
+###########################################
+## Regression
+###########################################
 
-X = Xdif
+X = balanced.drop(['time'], 1)
+y = balanced['time']
 
+# I played around with different feature inputs
+# sumcols = [col for col in balanced.columns if 'sum' in col]
+# difcols = [col for col in balanced.columns if 'dif' in col]
+# Xsum = balanced.drop(difcols + ['time'], 1)
+# Xdif = balanced.drop(sumcols + ['time'], 1)
+# X = Xsum
+# X = Xdif
+
+# I played around with other things too, but the code was way too messy
+
+# Anyway...train/test/val:
 X_train_val, X_test, y_train_val, y_test = \
         train_test_split(X, y, test_size=0.2,random_state=42)
 X_train, X_val, y_train, y_val = \
         train_test_split(X_train_val, y_train_val, test_size=.25, random_state=43)
 
-## Scale the data as before
+## Scale the data
 std = StandardScaler()
 std.fit(X_train.values)
-
-## Scale the Predictors on both the train and test set
 X_tr = std.transform(X_train.values)
 X_te = std.transform(X_test.values)
 
-# Run the cross validation, find the best alpha, refit the model on all the data with that alpha
-
-alphavec = 10**np.linspace(-2,2,200)
-
-lasso_model = LassoCV(alphas = alphavec, cv=5)
+# Cross-validate and re-fit
+alphavec = 10 ** np.linspace(-2, 2, 200)
+lasso_model = LassoCV(alphas=alphavec, cv=5)
 lasso_model.fit(X_tr, y_train)
 
-# This is the best alpha value it found - not far from the value
-# selected using simple validation
-lasso_model.alpha_
-
-# These are the (standardized) coefficients found
-# when it refit using that best alpha
+# Coefficients
 list(zip(X_train.columns, lasso_model.coef_))
 
-# Make predictions on the test set using the new model
-test_set_pred = lasso_model.predict(X_te)
-
-# Mean Absolute Error (MAE)
+# Mean Absolute Error, copied from notes
 def mae(y_true, y_pred):
     return np.mean(np.abs(y_pred - y_true))
 
-# Find the MAE and R^2 on the test set using this model
-mae(y_test, test_set_pred) # 4 min 26 seconds
+# Predict and score
+test_set_pred = lasso_model.predict(X_te)
+mae(y_test, test_set_pred)
+# 4 min 26 seconds wrong, on average
 r2_score(y_test, test_set_pred)
 
-#####################
+###########################################
+## LARS Path
+###########################################
 
-help(df\
-    .groupby(['f1'])\
-    .agg({
-        'fight_count': 'first',
-    }).hist)
-
-# PLOT
-fight_counts_agg = df\
-    .groupby(['f1'])\
-    .agg({
-        'fight_count': 'first',
-    })
-
-fight_counts_agg.hist()
-
-# Hide the right and top spines
-# ax.spines['right'].set_visible(False)
-# ax.spines['top'].set_visible(False)
-
-# sns.boxplot(fight_counts_agg)
-
-
-
-#####################
-
-from sklearn.linear_model import lars_path
-
-## Scale the variables
-std = StandardScaler()
-std.fit(X_train.values)
-
-X_tr = std.transform(X_train.values)
-
-## Note: lars_path takes numpy matrices, not pandas dataframes
-
-print("Computing regularization path using the LARS ...")
 alphas, _, coefs = lars_path(X_tr, y_train.values, method='lasso')
-
-# plotting the LARS path
-
 xx = np.sum(np.abs(coefs.T), axis=1)
 xx /= xx[-1]
 
-len(xx)
-len(X_train.columns)
-coefs.T.shape
+###########################################
+## Second Plot: LARS Path (all features)
+###########################################
 
-# PLOT
 plt.figure(figsize=(10,10))
 plt.plot(xx, coefs.T)
 ymin, ymax = plt.ylim()
 plt.vlines(xx, ymin, ymax, linestyle=None)
-# plt.xlabel('|coef| / max|coef|')
-# plt.ylabel('Coefficients')
 plt.title('LARS Path')
 plt.axis('tight')
 plt.legend(X_train.columns)
 plt.text(fontsize=25)
 plt.show()
 
-##
+###########################################
+## Third Plot: LARS Path (main features)
+###########################################
 
-zoom_col_indices = [9, 11, 13, 15]
 zoom_col_indices = [19, 21, 13, 15]
 xxx_cols = X_train.columns[zoom_col_indices]
 xxx_coefs = coefs.T[:, zoom_col_indices]
@@ -195,31 +158,21 @@ xxx_coefs = coefs.T[:, zoom_col_indices]
 xxx = np.sum(np.abs(xxx_coefs), axis=1)
 xxx /= xxx[-1]
 
-len(xxx)
-len(xxx_cols)
-xxx_coefs.shape
-
-# PLOT
-
 plt.figure(figsize=(10,10))
 plt.plot(xxx, xxx_coefs)
 ymin, ymax = plt.ylim()
-plt.vlines(xxx, ymin, ymax, linestyle='dashed')
-plt.xlabel('|coef| / max|coef|')
-plt.ylabel('Coefficients')
+plt.vlines(xxx, ymin, ymax, linestyle=None)
 plt.title('LASSO Path')
 plt.axis('tight')
 plt.legend(xxx_cols)
 plt.show()
 
-coefs.T[-1]
-
-##
+###########################################
+## Fourth Plot: Pairplot (main features)
+###########################################
 
 plot_cols = list(xxx_cols) + ['time']
-plot_cols
 
-# PLOT
 pp = sns.pairplot(balanced[plot_cols].rename(columns={
     'avg_win_time_sum': "",
     'avg_loss_time_sum': " ",
